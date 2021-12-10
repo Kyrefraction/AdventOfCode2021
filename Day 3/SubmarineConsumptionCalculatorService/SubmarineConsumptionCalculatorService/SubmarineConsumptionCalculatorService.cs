2af588e7dadb8fe7f39139c469910d288a66763a
Vincenzo <v.scialpi@hotmail.co.uk>
2021-12-10 15:26:17 +0000

    Removed redundant code

--- Day 3/SubmarineConsumptionCalculatorService/SubmarineConsumptionCalculatorService/SubmarineConsumptionCalculatorService.cs
+++ Day 3/SubmarineConsumptionCalculatorService/SubmarineConsumptionCalculatorService/SubmarineConsumptionCalculatorService.cs
@@ -1,16 +1,7 @@
-﻿using SubmarineConsumptionCalculatorService;
+using SubmarineConsumptionCalculatorService;
 
-namespace SubmarineCinsumptionCalculatorService
-{
-    public class SubmarineConsumptionCalculatorService
-    {
-        public static void Main(string[] args)
-        {
-            var rows = FileReader.ReadFile("input.txt");
-            var values = BitPositionValueParser.GetColumnValues(rows);
-            var powerConsumption = PowerConsumptionCalculator.CalculatePowerConsumption(values);
+var rows = FileReader.ReadFile("input.txt");
+var values = BitPositionValueParser.GetColumnValues(rows);
+var powerConsumption = PowerConsumptionCalculator.CalculatePowerConsumption(values);
 
-            Console.WriteLine(powerConsumption);
-        }
-    }
-}
+Console.WriteLine(powerConsumption);
