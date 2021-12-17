c1b7185d54d33f9c72e6634a0dcd36529ac8ba2c
Vincenzo <v.scialpi@hotmail.co.uk>
2021-12-17 15:31:27 +0000

    Update SubmarineConsumptionCalculatorService.cs

--- Day 3/SubmarineConsumptionCalculatorService/SubmarineConsumptionCalculatorService/SubmarineConsumptionCalculatorService.cs
+++ Day 3/SubmarineConsumptionCalculatorService/SubmarineConsumptionCalculatorService/SubmarineConsumptionCalculatorService.cs
@@ -3,5 +3,7 @@ using SubmarineConsumptionCalculatorService;
 var rows = FileReader.ReadFile("input.txt");
 var values = BitPositionValueParser.GetColumnValues(rows);
 var powerConsumption = PowerConsumptionCalculator.CalculatePowerConsumption(values);
+var lifeSupportRating = LifeSupportRatingCalculator.CalculateLifeSupportRating(values, rows);
 
-Console.WriteLine(powerConsumption);
+Console.WriteLine($"Power consumption: {powerConsumption}");
+Console.WriteLine($"Life support rating: {lifeSupportRating}");
