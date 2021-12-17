df294c5a32602db1d5330157b98aaf8896c3178b
Vincenzo <v.scialpi@hotmail.co.uk>
2021-12-17 15:57:00 +0000

    Part 2

--- Day 4/SquidBingo/SquidBingo/SquidBingo.cs
+++ Day 4/SquidBingo/SquidBingo/SquidBingo.cs
@@ -1,19 +1,29 @@
-﻿using SquidBingo;
+using SquidBingo;
 
 var numbers = FileReader.ReadNumbers("Numbers.txt");
 var boards = FileReader.ReadBoards("Boards.txt");
 
+var winningScore = new List<int>();
 foreach (var number in numbers)
 {
-    foreach (var board in boards)
+    foreach (var board in boards.ToList())
     {
         board.MarkOffNumber(number);
         if (board.HasLine())
         {
             var nonMarkedOffNumbers = board.GetNonMarkedOffNumbers();
             var score = nonMarkedOffNumbers.Sum() * number;
-            Console.WriteLine($"Winner has got score: {score}");
-            return;
+            
+            winningScore.Add(score);
+            boards.Remove(board);
         }
     }
+}
+
+for (var i = 0; i < winningScore.Count; i++)
+{
+    if (i == 0)
+        Console.WriteLine($"First winner has won with score: {winningScore[i]}");
+    if (i == winningScore.Count - 1)
+        Console.WriteLine($"Last winner has 'won' with score: {winningScore[i]}");
 }
