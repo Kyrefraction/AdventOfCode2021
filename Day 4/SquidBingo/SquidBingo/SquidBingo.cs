194643804af068e3e63716aee4e1d2627fc0eee6
Vincenzo <v.scialpi@hotmail.co.uk>
2021-12-17 16:19:10 +0000

    Refactor

--- Day 4/SquidBingo/SquidBingo/SquidBingo.cs
+++ Day 4/SquidBingo/SquidBingo/SquidBingo.cs
@@ -13,17 +13,12 @@ foreach (var number in numbers)
         {
             var nonMarkedOffNumbers = board.GetNonMarkedOffNumbers();
             var score = nonMarkedOffNumbers.Sum() * number;
-            
+
             winningScores.Add(score);
             boards.Remove(board);
         }
     }
 }
 
-for (var i = 0; i < winningScores.Count; i++)
-{
-    if (i == 0)
-        Console.WriteLine($"First winner has won with score: {winningScores[i]}");
-    if (i == winningScores.Count - 1)
-        Console.WriteLine($"Last 'winner' has 'won' with score: {winningScores[i]}");
-}
+Console.WriteLine($"First winner has won with score: {winningScores.First()}");
+Console.WriteLine($"Last 'winner' has 'won' with score: {winningScores.Last()}");
