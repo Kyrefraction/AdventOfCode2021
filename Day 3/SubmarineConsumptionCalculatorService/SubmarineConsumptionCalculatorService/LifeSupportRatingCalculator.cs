5fce405c473cb332c126ae3b29de91c683228867
Vincenzo <v.scialpi@hotmail.co.uk>
2021-12-17 15:32:13 +0000

    Add files via upload

---
+++ Day 3/SubmarineConsumptionCalculatorService/SubmarineConsumptionCalculatorService/LifeSupportRatingCalculator.cs
@@ -0,0 +1,16 @@
+﻿namespace SubmarineConsumptionCalculatorService
+{
+    public static class LifeSupportRatingCalculator
+    {
+        public static int CalculateLifeSupportRating(Dictionary<int, List<string>> values, List<string> rows)
+        {
+            var oxygenGeneratorRatingBinary = DiagnosticNumberCalculator.GetOxygenGeneratorRating(values, rows);
+            var carbonDioxideScrubberRatingBinary = DiagnosticNumberCalculator.GetCarbonDioxideScrubberRating(values, rows);
+
+            var oxygenGeneratorRatingDenary = Convert.ToInt32(oxygenGeneratorRatingBinary, 2);
+            var carbonDioxideGeneratorRatingDenary = Convert.ToInt32(carbonDioxideScrubberRatingBinary, 2); 
+
+            return oxygenGeneratorRatingDenary * carbonDioxideGeneratorRatingDenary;
+        }
+    }
+}
